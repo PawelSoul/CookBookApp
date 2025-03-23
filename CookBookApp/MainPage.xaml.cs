46d0e549b7c31f916d34eb6a9a117fddaaaec5b3
Paweł Dusza <soul.pawel@gmail.com>
2025-03-23 21:11:35 +0100

    poprawki estetyczne dla MainPage

--- CookBookApp/MainPage.xaml.cs
+++ CookBookApp/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CommunityToolkit.Maui.Views;
 using CookBookApp.Data;
 using CookBookApp.Models;
 using CookBookApp.Repositories;
@@ -130,5 +131,56 @@ namespace CookBookApp
                 SearchResultsLayout.Children.Add(recipeCard);
             }
         }
+        private void OnFilterTapped(object sender, EventArgs e)
+        {
+            if (sender is HorizontalStackLayout layout)
+            {
+                foreach (var child in layout.Children)
+                {
+                    if (child is CheckBox checkbox)
+                    {
+                        checkbox.IsChecked = !checkbox.IsChecked;
+                        break;
+                    }
+                }
+            }
+        }
+        private async void OnHeaderPointerEntered(object sender, PointerEventArgs e)
+        {
+            if (sender is Label label)
+                await label.ScaleTo(1.1, 100); // Powiększenie
+        }
+
+        private async void OnHeaderPointerExited(object sender, PointerEventArgs e)
+        {
+            if (sender is Label label)
+                await label.ScaleTo(1.0, 100); // Powrót
+        }
+        // RODZAJ DANIA
+        private async void OnRodzajDaniaPointerEntered(object sender, PointerEventArgs e) => await RodzajDaniaLabel.ScaleTo(1.1, 100);
+        private async void OnRodzajDaniaPointerExited(object sender, PointerEventArgs e) => await RodzajDaniaLabel.ScaleTo(1.0, 100);
+        private void OnRodzajDaniaExpandedChanged(object sender, EventArgs e) => RodzajDaniaArrow.Text = RodzajDaniaExpander.IsExpanded ? "▲" : "▼";
+
+        // KUCHNIA ŚWIATA
+        private async void OnKuchniaPointerEntered(object sender, PointerEventArgs e) => await KuchniaLabel.ScaleTo(1.1, 100);
+        private async void OnKuchniaPointerExited(object sender, PointerEventArgs e) => await KuchniaLabel.ScaleTo(1.0, 100);
+        private void OnKuchniaExpandedChanged(object sender, EventArgs e) => KuchniaArrow.Text = KuchniaExpander.IsExpanded ? "▲" : "▼";
+
+        // GŁÓWNY SKŁADNIK
+        private async void OnSkladnikPointerEntered(object sender, PointerEventArgs e) => await SkladnikLabel.ScaleTo(1.1, 100);
+        private async void OnSkladnikPointerExited(object sender, PointerEventArgs e) => await SkladnikLabel.ScaleTo(1.0, 100);
+        private void OnSkladnikExpandedChanged(object sender, EventArgs e) => SkladnikArrow.Text = SkladnikExpander.IsExpanded ? "▲" : "▼";
+
+        // OKAZJA
+        private async void OnOkazjaPointerEntered(object sender, PointerEventArgs e) => await OkazjaLabel.ScaleTo(1.1, 100);
+        private async void OnOkazjaPointerExited(object sender, PointerEventArgs e) => await OkazjaLabel.ScaleTo(1.0, 100);
+        private void OnOkazjaExpandedChanged(object sender, EventArgs e) => OkazjaArrow.Text = OkazjaExpander.IsExpanded ? "▲" : "▼";
+
+        // STOPIEŃ TRUDNOŚCI
+        private async void OnTrudnoscPointerEntered(object sender, PointerEventArgs e) => await TrudnoscLabel.ScaleTo(1.1, 100);
+        private async void OnTrudnoscPointerExited(object sender, PointerEventArgs e) => await TrudnoscLabel.ScaleTo(1.0, 100);
+        private void OnTrudnoscExpandedChanged(object sender, EventArgs e) => TrudnoscArrow.Text = TrudnoscExpander.IsExpanded ? "▲" : "▼";
+
+
     }
 }
